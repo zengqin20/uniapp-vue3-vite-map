8df69c22e73f94d0d8d35dd8e68013ff56fe2093
zengqin20 <zengqin20@163.com>
2022-03-18 18:15:27 +0800

    feat: 新增首页界面

---
+++ src/pages/common/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <view class="content">
+    <text class="iconfont icon-laonianren"></text>
+    <text class="iconfont icon-zinv"></text>
+  </view>
+  <view class="text">银发易出行</view>
+</template>
+<script setup>
+// import { onLoad, onShow, onLaunch } from '@dcloudio/uni-app'
+// import { api, postApi } from '@/request/index.js'
+</script>
+
+<style>
+.iconfont {
+  color: #fff;
+  font-size: 38vw;
+}
+page {
+  background-image: linear-gradient(#ff6647, #f09819, #f1f1f1);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  top: 15%;
+  position: relative;
+  margin-top: -39px;
+}
+.text {
+  margin-left: 27px;
+  font-size: 14vw;
+  margin-top: 56px;
+  color: #fff;
+}
+</style>
