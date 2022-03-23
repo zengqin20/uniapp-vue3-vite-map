3cb69522333e73b802a84d9609d26ddcb5140ea5
zengqin20 <zengqin20@163.com>
2022-03-23 10:36:27 +0800

    feat: 新增子女模块相关功能页面

--- src/pages/child/index.vue
+++ src/pages/child/index.vue
@@ -1,10 +1,28 @@
 <template>
-  <view class="content flex-center"> </view>
+  <view class="content flex-center">
+    <history v-if="activeItem === 'history'"></history>
+    <location v-if="activeItem === 'location'"></location>
+    <sync v-if="activeItem === 'sync'"></sync>
+  </view>
+  <van-tabbar :active="activeItem" @change="onChange" active-color="#ff6647">
+    <van-tabbar-item name="location" icon="friends-o">家人</van-tabbar-item>
+    <van-tabbar-item name="history" icon="search">历史轨迹</van-tabbar-item>
+    <van-tabbar-item name="sync" icon="setting-o">同步</van-tabbar-item>
+  </van-tabbar>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import { api, getApi, postApi } from '@/request/index.js'
+import history from '@/pages/child/history'
+import sync from '@/pages/child/sync'
+import location from '@/pages/child/location'
+
+const activeItem = ref('location')
+
+const onChange = e => {
+  activeItem.value = e.detail
+}
 </script>
 
 <style lang="less">
---
+++ src/pages/child/sync.vue
@@ -0,0 +1,18 @@
+<template>
+  <view> 12131231 </view>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { api, getApi, postApi } from '@/request/index.js'
+
+onMounted(() => {
+  console.log(12)
+})
+</script>
+
+<style lang="less">
+page {
+  background-color: #f1f1f1;
+}
+</style>
