f12fac35579bb976bf2c907349253153887a64bb
zengqin20 <zengqin20@163.com>
2022-03-23 10:41:29 +0800

    feat: 新增绑定用户bar

--- src/pages/child/index.vue
+++ src/pages/child/index.vue
@@ -3,11 +3,13 @@
     <history v-if="activeItem === 'history'"></history>
     <location v-if="activeItem === 'location'"></location>
     <sync v-if="activeItem === 'sync'"></sync>
+    <bind-parent v-if="activeItem === 'bindparent'"></bind-parent>
   </view>
   <van-tabbar :active="activeItem" @change="onChange" active-color="#ff6647">
-    <van-tabbar-item name="location" icon="friends-o">家人</van-tabbar-item>
+    <van-tabbar-item name="location" icon="friends-o">实时定位</van-tabbar-item>
     <van-tabbar-item name="history" icon="search">历史轨迹</van-tabbar-item>
     <van-tabbar-item name="sync" icon="setting-o">同步</van-tabbar-item>
+    <van-tabbar-item name="bindparent" icon="friends-o">家人</van-tabbar-item>
   </van-tabbar>
 </template>
 
@@ -17,6 +19,7 @@ import { api, getApi, postApi } from '@/request/index.js'
 import history from '@/pages/child/history'
 import sync from '@/pages/child/sync'
 import location from '@/pages/child/location'
+import bindParent from '@/pages/child/bindParent'
 
 const activeItem = ref('location')
 
