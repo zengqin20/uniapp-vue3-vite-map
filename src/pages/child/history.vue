b6e3d2c875f91d2a5f8c965e99d63aa003b47aab
zengqin20 <zengqin20@163.com>
2022-04-12 10:24:40 +0800

    fix: 历史轨迹组件

--- src/pages/child/history.vue
+++ src/pages/child/history.vue
@@ -1,10 +1,16 @@
 <template>
-  <view> 12131231 </view>
+  <map :markers="markers" :polyline="polyline" :longitude="longitude" :latitude="latitude"></map>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
 import { api, getApi, postApi } from '@/request/index.js'
+
+const makers = ref([])
+const polyline = ref([])
+const longitude = ref('')
+const latitude = ref('')
+const positionArr = ref([])
 
 onMounted(() => {
   console.log(12)
