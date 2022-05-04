4e5b764f8cfa21f9b12eb5e4593dc00c84e6186c
zengqin20 <zengqin20@163.com>
2022-05-04 09:55:48 +0800

    feat: 新增历史轨迹

--- src/pages/child/history.vue
+++ src/pages/child/history.vue
@@ -1,24 +1,85 @@
 <template>
-  <map :markers="markers" :polyline="polyline" :longitude="longitude" :latitude="latitude"></map>
+  <view class="text" v-if="!isLocation">
+    <text class="iconfont icon-meiyoushuju"></text>
+    <text class="text-info">请先去绑定相关用户</text>
+  </view>
+
+  <view v-else class="users"
+    ><text class="bind-text">可查看历史轨迹</text>
+    <view class="bind-user" v-for="row in names" @click="handleRoute(row.location)">
+      {{ row.name }}</view
+    >
+  </view>
 </template>
 
 <script setup>
 import { ref, onMounted } from 'vue'
-import { api, getApi, postApi } from '@/request/index.js'
+import { api, getApi } from '@/request/index.js'
+import { handleLocation } from '@/utils/common'
 
-const makers = ref([])
-const polyline = ref([])
-const longitude = ref('')
-const latitude = ref('')
-const positionArr = ref([])
+const isLocation = ref(false)
+const names = ref([])
+
+const handleRoute = location => {}
+
+const getLocation = () => {
+  getApi(api.location).then(res => {
+    if (res.data.isLocation) {
+      isLocation.value = true
+      names.value = res.data.infos
+    } else {
+      isLocation.value = false
+    }
+  })
+}
 
 onMounted(() => {
-  console.log(12)
+  getLocation()
 })
 </script>
 
 <style lang="less">
+.users {
+  background-image: linear-gradient(#ff6647, #f09819, #f1f1f1);
+  padding: 24px;
+  height: 16vw;
+  display: flex;
+  align-items: center;
+  .bind-user {
+    margin-left: 16px;
+    width: 14vw;
+    border: 1px solid;
+    border-radius: 12px;
+    display: flex;
+    justify-content: center;
+    height: 7vw;
+    align-items: center;
+  }
+  .iconfont {
+    font-size: 6vw;
+    color: #8c7070;
+  }
+}
 page {
   background-color: #f1f1f1;
+  color: #fff;
+}
+.content {
+  background-image: linear-gradient(#ff6647, #f09819, #f1f1f1);
+}
+
+.text {
+  color: #8c7070;
+  margin-top: 54px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  .icon-meiyoushuju {
+    font-size: 32vw;
+    margin-bottom: 24px;
+  }
+  .text-info {
+    font-size: 6vw;
+  }
 }
 </style>
